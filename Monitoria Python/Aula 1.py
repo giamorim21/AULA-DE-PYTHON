e2f7ab72c91f4348202c78c43cbff5a368589b74
giamorim21 <126572207+giamorim21@users.noreply.github.com>
2023-05-23 14:02:15 -0300

    Add files via upload

---
+++ Monitoria Python/Aula 1.py
@@ -0,0 +1,32 @@
+# \n : pula linha
+# \t : dá espaço de tab
+# = : operador de atribuição
+# ** : operador de exponenciação
+
+print("Hello World")
+print("\tPrimeira aula")
+
+a = 5
+a += 5
+print(a)
+
+x = 10
+y = 5
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+
+#concatenação : junção das strings
+print('a' + 'b')
+print('a' * 3)
+
+#input() : faz com que o usuário possa incerir um valor como string
+#b = int(input("Digite um valor para salvar: "))
+#print(b)
+
+w = 3.5
+j = 6.8
+print(float(w) + int(j))
